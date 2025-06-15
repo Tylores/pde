52686e160c336bc906bcedb04272c9cf27027cf6
Tylores <tylor.slay@gmail.com>
2025-06-15 12:16:43 -0700

    fix: nvim needs python venv to install lsp and formatters

--- tools/setup_python.sh
+++ tools/setup_python.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install curl python3 python3-pip
+sudo apt install curl python3 python3-pip python3-venv
 
 curl -sSL https://install.python-poetry.org | python3 -
 poetry self update
