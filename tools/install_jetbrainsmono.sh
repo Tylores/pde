bfccb62e63930954c94ea6477692a764e5b69490
Tylores <tylor.slay@gmail.com>
2026-01-19 10:00:26 -0800

    fix: moved jet brains install and install tree-sitter-cli

---
+++ tools/install_jetbrainsmono.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.2/JetBrainsMono.zip \
+&& cd ~/.local/share/fonts \
+&& unzip JetBrainsMono.zip \
+&& rm JetBrainsMono.zip \
+&& fc-cache -fv
--- tools/setup_nvim.sh
+++ tools/setup_nvim.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# requirments
-sudo add-apt-repository ppa:neovim-ppa/stable -y
+# nvim requirments
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
-sudo apt install make gcc ripgrep unzip git xclip neovim
+sudo apt install make gcc build-essential libreadline-dev ripgrep unzip git xclip npm luarocks neovim
+sudo npm install -g tree-sitter-cli
 
-# optional
-wget -P ~/.local/share/fonts https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip \
-&& cd ~/.local/share/fonts \
-&& unzip JetBrainsMono.zip \
-&& rm JetBrainsMono.zip \
-&& fc-cache -fv
-
+# kickstart 
 sudo rm -rf ~/.config/nvim
+sudo rm -rf ~/.local/share/nvim/lazy
+sudo rm -rf ~/.local/state/nvim/lazy
+sudo rm -rf ~/.config/nvim/lazy-lock.json
 git clone https://github.com/Tylores/kickstart.nvim.git "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
