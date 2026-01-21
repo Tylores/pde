41bec1d0fde6ce55f55543076d3e505a641ec073
Tylores <tylor.slay@gmail.com>
2026-01-21 15:16:37 -0800

    fix: golang setup didn't append path correctly

--- tools/setup_golang.sh
+++ tools/setup_golang.sh
@@ -5,7 +5,6 @@ sudo apt install pkg-config libhunspell-dev
 wget https://go.dev/dl/go1.25.6.linux-amd64.tar.gz
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go1.25.6.linux-amd64.tar.gz
-echo export PATH=$PATH:/usr/local/go/bin >> ~/.bashrc
-echo PATH="/usr/local/go/bin:$PATH" >> ~/.bashrc
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.bashrc
 source ~/.bashrc
 go version
